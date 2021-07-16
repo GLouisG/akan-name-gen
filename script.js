1b413c51654866184fe3eb086f1a3baed8e1e867
GLouisG <louis.gacho@student.moringaschool.com>
2021-07-16 15:20:32 +0300

    declare birth month and birth date variables

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 let form = document.getElementById('form');
 let birthYear = document.getElementById('birth-year');
+let birthMonth = document.getElementById('birth-month');
+let birthMonth = document.getElementById('birth-date');
