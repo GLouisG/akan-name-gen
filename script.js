452b842452d25ef9648b1b3f88f55b17d57ace0d
GLouisG <louis.gacho@student.moringaschool.com>
2021-07-16 22:15:41 +0300

    created validate function

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-let form = document.getElementById('form');
-let birthYear = document.getElementById('birth-year');
-let birthMonth = document.getElementById('birth-month');
-let birthDate = document.getElementById('birth-date');
+var form = document.getElementById("form");
+    var bYear = document.getElementById("birth-year");
+    var bMonth = document.getElementById("birth-month");
+    var bDate = document.getElementById("birth-date");
 
-var bYear = parseInt(birthYear);
-var bMonth = parseInt(birthMonth);
-var bDate = parseInt(birthDate);
+function validate(bYear, bMonth, bDate){
+    console.log("reaches here");
+  var bYear1 = parseInt(bYear.value);
+  var bMonth1 = parseInt(bMonth.value);
+   var bDate1 = parseInt(bDate.value);
 
-function validation(){
-    if(bYear<1){
-        return false;
+
+    if(bYear1<1){
         alert("Wrong birth details");
-    }else if(bDate<1 || bDate>31){
         return false;
+    }
+    if(bDate1<1 || bDate>31){
         alert("Wrong birth details");
+        return false;
+    }else{
+        true;
     }
 }
