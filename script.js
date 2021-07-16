a894887359388fdcd19c002ac4bc952c9b05aeaf
GLouisG <louis.gacho@student.moringaschool.com>
2021-07-16 15:46:38 +0300

    Spacing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let form = document.getElementById('form');
+
 let birthYear = document.getElementById('birth-year');
 let birthMonth = document.getElementById('birth-month');
 let birthMonth = document.getElementById('birth-date');
